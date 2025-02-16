ed9574689865a46c4dc52ba0d3cf80bfc4d6e19f
Andrea Katherine <jaimeandreak@gmail.com>
2025-02-16 22:53:58 +0100

    cambios para que funcione con render2

--- server2.js
+++ server2.js
@@ -1,8 +1,5 @@
-require("dotenv").config(); // 🔹 Cargar variables de entorno
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-
 const db = require("./src/db"); // Importamos la conexión
 
 const app = express();
@@ -23,9 +20,7 @@ app.get("/frase-aleatoria", (req, res) => {
 });
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 3001;
-
-//----------------------------------------------
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
