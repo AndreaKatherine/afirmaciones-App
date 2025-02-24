969a60fab45696ad745b251d4b45e819fdf85ac3
Andrea Katherine <jaimeandreak@gmail.com>
2025-02-24 11:43:58 +0100

    se agrega el proyecto

--- bd_fra.sql
+++ bd_fra.sql
@@ -1,9 +1,11 @@
 CREATE DATABASE frases_db;
 USE frases_db;
+
 CREATE TABLE FRASES (
 id int auto_increment primary key,
 texto varchar (250) NOT NULL
 );
+
 INSERT INTO FRASES (TEXTO) VALUES
 ('El éxito es la suma de pequeños esfuerzos repetidos día tras día.');
 SELECT * FROM frases;
